214010d9ee88811a60f114a08a1c283bf059d266
Satu <satu.salekari@gmail.com>
2019-10-23 16:29:03 +0300

    Initial evaluator page

---
+++ frontend/src/pages/evaluator/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Container from "../../components/Container";
+import Page from "../../components/Page";
+import IndexLayout from "../../layouts";
+
+export default function EvaluatorPage() {
+
+  return (
+    <IndexLayout>
+      <Page>
+        <Container>
+          <h1>Evaluate typographic elements</h1>
+        </Container>
+      </Page>
+    </IndexLayout>
+  );
+}
