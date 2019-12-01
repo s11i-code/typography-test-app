84b48f8c81ab5d5ad4a2a21e8c3791f17f2c837e
Satu <satu.salekari@gmail.com>
2019-12-01 15:35:18 +0200

    Change hostname

--- backend/common/index.ts
+++ backend/common/index.ts
@@ -29,23 +29,8 @@ export function getS3FolderPath(site: string, resolution: Resolution): string {
         path =`${resolution.width}x${resolution.height}`;
     }
     if(site) {
-        const cleanedSite = site.replace("http://", "").replace("https://", "");
-        path =`${path}/${extractHostname(cleanedSite)}`;
+        const cleanedSite = site.replace("http://", "").replace("https://", "").replace('/', '-').replace('?', '-').replace('&', '-');
+        path =`${path}/${cleanedSite}`;
     }
     return path;
-}
-
-function extractHostname(url:string): string {
-    let hostname:string;
-
-    if (url.indexOf("//") > -1) {
-        hostname = url.split('/')[2];
-    }
-    else {
-        hostname = url.split('/')[0];
-    }
-    hostname = hostname.split(':')[0];  //find & remove port number
-    hostname = hostname.split('?')[0];     //find & remove "?"
-
-    return hostname;
 }
